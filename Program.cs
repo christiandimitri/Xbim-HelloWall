e3f79b437f688fd2608f926d5ddbc2906fa02fb8
Christian <christian.j.dimitrii@gmail.com>
2020-09-22 00:52:36 +0200

    First Commit

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace HelloWall
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            int number = 1+1;
+            Console.WriteLine("number: {0}",number);
+        }
+    }
+}
